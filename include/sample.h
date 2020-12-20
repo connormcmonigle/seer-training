971988155f5b0f0eb53526b0824b5d88f7c637dc
Connor McMonigle <connormcmonigle@gmail.com>
2020-12-20 12:56:54 -0800

    init

---
+++ include/sample.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iomanip>
+#include <iostream>
+#include <cstdint>
+
+#include <compat.h>
+
+namespace train{
+
+struct sample{
+    compat::state_type state{};
+
+    compat::score_type win_;
+    compat::score_type draw_;
+    compat::score_type loss_;
+
+    double win() const { return static_cast<double>(win_) / static_cast<double>(compat::wdl_scale); }
+    double draw() const { return static_cast<double>(draw_) / static_cast<double>(compat::wdl_scale); }
+    double loss() const { return static_cast<double>(loss_) / static_cast<double>(compat::wdl_scale); }
+
+};
+
+
+std::ostream& operator<<(std::ostream& ostr, const sample& x){
+  return ostr << x.bd.fen() << '|' << x.win_ << '|' << x.draw_ << '|' << x.loss_ << '|';
+}
+
+
+
+}
