53a55e5babb05d51877ba9698b6e790445e31966
Connor McMonigle <connormcmonigle@gmail.com>
2021-01-05 16:04:40 -0800

    improvements

--- scripts/train.py
+++ scripts/train.py
@@ -3,6 +3,7 @@ import torch
 import torch.optim as optim
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
+import argparse
 
 import config
 import seer_train
@@ -28,19 +29,24 @@ def train_step(M, sample, opt, queue, max_queue_size, report=False):
 
 
 def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('resume', type=int, default=util.valid_man_counts()[0], help='piece count to resume training from')
+  args = parser.parse_args()
+
   cfg = config.Config('config.yaml')
 
   sample_to_device = lambda x: tuple(map(lambda t: t.to(cfg.device, non_blocking=True), x))
 
   M = model.NNUE().to(cfg.device)
 
-  if (path.exists(cfg.model_save_path)):
-    print('Loading model ... ')
-    M.load_state_dict(torch.load(cfg.model_save_path))
-
   valid_man_counts = util.valid_man_counts()
 
   sess = seer_train.Session(cfg.root_path).set_concurrency(cfg.concurrency)
+
+  if (path.exists(cfg.model_save_path)):
+    print('Loading model ... ')
+    M.load_state_dict(torch.load(cfg.model_save_path))
+    sess.load_weights(cfg.bin_model_save_path)
 
   #train_data = dataset.SeerData(sess.get_n_man_train_reader(valid_man_counts[0]), cfg)
   
@@ -61,6 +67,14 @@ def main():
   total_steps = 0
 
   for men in valid_man_counts:
+    
+    if men < args.resume:
+      assert(path.exists(seer_train.train_n_man_path(cfg.root_path, men)))
+      total_steps += sess.get_n_man_train_reader(men).size() // cfg.batch_size
+      print('skipping {} man positions'.format(men))
+      continue
+    else:
+      print('training on {} man positions'.format(men))
     
     train_data = dataset.SeerData(sess.get_n_man_train_reader(men), cfg)
   
